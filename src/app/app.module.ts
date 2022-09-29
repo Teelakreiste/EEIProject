4bd8438328a7bfdd28cbc6102c5c791b70af24ab
Teelakreiste <71708924+Teelakreiste@users.noreply.github.com>
2022-09-29 10:39:24 -0500

    Compontentes
    Creando los compontentes principales del programa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RegisterAdditionalComponent } from './register-additional/register-additional.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    RegisterAdditionalComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/register-additional/register-additional.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-additional',
+  templateUrl: './register-additional.component.html',
+  styleUrls: ['./register-additional.component.css']
+})
+export class RegisterAdditionalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
